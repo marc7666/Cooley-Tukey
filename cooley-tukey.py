53fb52b95d99b58685fd83e9cc3707aa028694de
Marc Cervera <64266158+marc7666@users.noreply.github.com>
2023-01-02 16:48:35 +0100

    Update cooley-tukey.py

--- cooley-tukey.py
+++ cooley-tukey.py
@@ -1,7 +1,3 @@
-"""
-__author__ = Marc Cervera Rosell
-__date__ = 02/01/2023
-"""
 import numpy as np
 
 
@@ -14,10 +10,10 @@ def fft(x):
     N = len(x)  # Signal size
     if N == 1:  # Base case: if the signal has size 1, the signal itself is returned
         return x
-    even = fft(x[0::2])  # Calculating the DFT for the even part of the signal
-    odd = fft(x[1::2])  # Calculating the DFT for the odd part of the signal
-    T = [np.exp(-2j * np.pi * k / N) * odd[k] for k in range(N // 2)]  # Calculating the term T
-    return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(
+    even_positions = fft(x[0::2])  # Calculating the DFT for the even part of the signal
+    odd_positions = fft(x[1::2])  # Calculating the DFT for the odd part of the signal
+    T = [np.exp(-2j * np.pi * k / N) * odd_positions[k] for k in range(N // 2)]  # Calculating the term T
+    return [even_positions[k] + T[k] for k in range(N // 2)] + [even_positions[k] - T[k] for k in range(
         N // 2)]  # Joining the results to calculate the complete DFT
 
 
